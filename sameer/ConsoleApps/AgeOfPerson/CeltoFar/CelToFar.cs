8a587860d1b3e8caaee78d8a2c380dade941b45e
JamSameer <sameer@jamsons.in>
2016-11-04 19:29:34 +0530

    sameer added his projects

---
+++ sameer/ConsoleApps/AgeOfPerson/CeltoFar/CelToFar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeltoFar
+{
+    class CelToFar
+    {
+        static void Main(string[] args)
+        {
+            double cel, far;
+            Console.WriteLine("Enter the to convert temperatures from Celsius \x00B0C to Fahrenheit \x00B0F");
+            cel = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Entered Temprature in Celsius is:" + cel + "\x00B0");
+            far = (1.8 * cel) + 32;
+            Console.WriteLine("The convertd Temprature from Celsius \x00B0C to Fahrenheit \x00B0F is:" + far + "\x00B0");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sameer/ConsoleApps/AgeOfPerson/Ordinals/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordinals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, k;
+            Console.WriteLine("Entter the Number");
+            k = int.Parse(Console.ReadLine());
+            i = k % 10;
+            
+            if(i==1)
+            {
+                Console.WriteLine("That's the" + k + "st item");
+            }
+            else if (i== 2)
+            {
+                Console.WriteLine("That's the" + k + "nd item");
+            }
+            else if (i == 3)
+            {
+                Console.WriteLine("That's the" + k + "rd item");
+            }
+            //else if (i == 4)
+            //{
+            //    Console.WriteLine("That's the" + k + "th item");
+            //}
+            else
+            {
+                Console.WriteLine("That's the" + k + "th item");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
