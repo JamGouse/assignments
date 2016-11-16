4121d9bbaa237c4805db2994a20f82a62075f545
JamGouse <gouse@jamsons.in>
2016-11-16 11:06:31 +0530

    Array assignments

---
+++ grades/ConsoleArrayApp/EvenNumbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many numbers do you want to enter? :");
+            int numbers = int.Parse(Console.ReadLine());
+            int[] array = new int[numbers];
+            Console.WriteLine("Enter " + numbers + " numbers");
+            int item = 0;
+            item = PrintArray(array, item);
+            Console.WriteLine("Even numbers are :");
+            EvenNumber(array);
+            Console.ReadLine();
+        }
+
+        private static void EvenNumber(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    Console.WriteLine("{0}", array[i]);
+                }
+
+            }
+        }
+
+        private static int PrintArray(int[] array, int item)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                item = int.Parse(Console.ReadLine());
+                array.SetValue(item, i);
+            }
+            return item;
+        }
+    }
+}
---
+++ grades/ConsoleArrayApp/RecursivePalindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursivePalindrome
+{
+    class Program
+    {
+        string str = null;
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string : ");
+            string str = Console.ReadLine();
+            bool result = isPalindrom(str);
+            if (result)
+            {
+                Console.WriteLine(str.ToUpper() + " is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine(str.ToUpper() + " is not a palindrome");
+            }
+            Console.ReadLine();
+        }
+
+        private static bool isPalindrom(string str)
+        {
+            if (str.Length == 0 || str.Length == 1)
+            {
+                return true;
+            }
+            else
+            {
+                if (str.ElementAt(0) != str.ElementAt(str.Length - 1))
+                {
+                    return false;
+                }
+                else
+                {
+                    return isPalindrom(str.Substring(1, str.Length - 2));
+                }
+            }
+        }
+    }
+}
